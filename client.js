7bf91e93ba3c4d21ec03d884607553ea74819480
naveenr414 <dsfan414@gmail.com>
2018-05-03 14:40:59 -0400

    Uses sockets to play with multiple players

--- client.js
+++ client.js
@@ -2,16 +2,18 @@ var grid = 0;
 var turn = false;
 var placed = 0;
 var totalPlace = 3;
-var colors = [0,"#FF0000","#0000FF"];
+var colors = [];
 
 var size = 30;
-var width = 800;
-var height = 270;
+var width = 300;
+var height = 300;
 
 var mouseX = 0;
 var mouseY = 0;
 var mousePress = false;
 var socket = io();
+
+var number = 0;
 
 $(document).mousemove(function(e) {
     mouseX = e.pageX;
@@ -35,7 +37,6 @@ function drawGrid()
 		for(var x = 0;x<grid.length;x++)
 		{	
 			if(grid[y][x]!=0){
-				console.log("Here");
 				c.fillStyle=colors[grid[y][x]];
 				c.fillRect(size*x,size*y,size,size);
 			}
@@ -44,29 +45,60 @@ function drawGrid()
 
 	c.stroke();
 }
-if(turn){
-	var gridX = Math.floor(mouseX/size);
-	var gridY = Math.floor(mouseY/size);
-	
-	if(gridX<width/size && gridY<height/size)
+
+function checkInput()
+{
+	console.log(turn);
+	if(turn)
 	{
-		c.fillStyle="#AAAAAA";
-		c.fillRect(gridX*size,gridY*size,size,size);
-				
-		if(mousePress && grid[gridY][gridX] == 0)
+		drawGrid();
+		var c = document.getElementById("mainCanvas").getContext("2d");	
+		var gridX = Math.floor(mouseX/size);
+		var gridY = Math.floor(mouseY/size);
+		
+		if(gridX<=width/size && gridY<=height/size)
 		{
-			grid[gridY][gridX] = 1;
-			placed+=1;
-			
-			if(placed == totalPlace)
+			c.fillStyle="#AAAAAA";
+			c.fillRect(gridX*size,gridY*size,size,size);
+					
+			if(mousePress && grid[gridY][gridX] == 0)
 			{
-				turn = false;
-			}
-		}	
+				grid[gridY][gridX] = number;
+				placed+=1;
+				socket.emit ('move',[gridY,gridX,number]);
+				
+				console.log(placed+" "+totalPlace);
+				if(placed == totalPlace)
+				{
+					console.log("Done");
+					turn = false;
+				}
+				
+				console.log("Turn is "+turn);
+				
+			}	
+		}
 	}
 }
 
 socket.on('grid',function(g){
 	grid = g;
 	drawGrid();
+	console.log("Grid recieved");
 });
+
+socket.on('color',function(c){
+
+	colors = c;
+	drawGrid();
+});
+
+socket.on('input', function(){
+	turn = true;
+});
+
+socket.on('number', function(n){
+	number = n;
+});
+
+setInterval(checkInput,20);
--- server.js
+++ server.js
@@ -3,6 +3,29 @@ var express = require("express");
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var colors = [0,"#FF0000","#0000FF"];
+var inputRecieved = [];
+var totalInput = 3;
+var stage = "Update";
+var grid = [];
+
+var lastTime = 0;
+var stage = "Updating";
+var ticks = 0;
+var updatesPer = 5;
+
+inputRecieved.push(totalInput);
+
+for(var i = 0;i<10;i++)
+{
+	var temp = [];
+	for(var j = 0;j<10;j++)
+	{
+		temp.push(Math.floor(Math.random()*3));
+	}
+	grid.push(temp);
+}
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -11,25 +34,141 @@ app.use('/', express.static(__dirname + '/'));
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  
+  socket.emit("number",inputRecieved.length);
+  inputRecieved.push(0);
+ 
+  io.emit('color',colors);
+  
+  socket.on('move', function(info){
+	console.log("Move "+info);
+	if(inputRecieved[info[2]]!=totalInput && grid[info[0]][info[1]]==0)
+	{
+		grid[info[0]][info[1]] = info[2];
+		io.emit("grid",grid);
+		inputRecieved[info[2]]+=1;
+	}
+	});
 });
+
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
-function createRandom()
+function update()
 {
-	var grid = [];
-	for(var i = 0;i<10;i++)
+	if(stage=="Updating")
 	{
-		var temp = [];
-		for(var j = 0;j<10;j++)
-		{
-			temp.push(Math.floor(3*Math.random()));
-		}
-		grid.push(temp);
+		console.log("Made a move");
+		updateGrid();
+		ticks+=1;
+		lastTime = new Date().getTime();
+		stage = "Waiting";
+		io.emit("grid",grid);
 	}
-	io.emit("grid",grid);
+	else if(stage=="Waiting")
+	{
+		var time = new Date().getTime();
+		if(time-lastTime>1000)
+		{
+			if(ticks == updatesPer)
+			{
+				ticks = 0;
+				io.emit("input");
+				stage = "Input";
+			}
+			else
+			{
+				stage = "Updating";
+			}
+		}
+	}
+	else if(stage=="Input")
+	{
+		var done = true;
+		for(var i = 0;i<inputRecieved.length;i++)
+		{
+			done&=inputRecieved[i]==totalInput;
+		}
+		
+		
+		if(done)
+		{
+			stage = "Waiting";
+			lastTime = new Date().getTime();
+			for(var i = 0;i<inputRecieved.length;i++)
+			{
+				inputRecieved[i] = 0;
+			}
+			inputRecieved[0] = totalInput;
+		}
+	}
 }
 
-setInterval(createRandom,1000);
+function updateGrid()
+{
+	var newGrid = [];
+	for(var i = 0;i<grid.length;i++)
+	{
+		var temp = [];
+		for(var j = 0;j<grid[0].length;j++)
+		{
+			temp.push(0);
+		}
+		newGrid.push(temp);
+	}
+
+	for(var i = 0;i<newGrid.length;i++)
+	{
+		for(var j = 0;j<newGrid[0].length;j++)
+		{
+			var neighbors = 0;
+			for(var xPlus = -1;xPlus<=1;xPlus++)
+			{
+				for(var yPlus = -1;yPlus<=1;yPlus++)
+				{
+					if(j+xPlus>=0 && j+xPlus<newGrid[0].length
+					&& i+yPlus>=0 && i+yPlus<newGrid.length)
+					{
+						neighbors+=grid[i+yPlus][j+xPlus]==grid[i][j];
+					}
+				}
+			}
+			
+			neighbors-=grid[i][j]>0;
+			
+			if(grid[i][j]==0)
+			{
+				if(neighbors==3)
+				{
+					newGrid[i][j] = grid[i][j];
+				}
+				else
+				{
+					newGrid[i][j] = 0;
+				}
+			}
+			else
+			{
+				if(neighbors==2 || neighbors==3)
+				{
+					newGrid[i][j] = grid[i][j];
+				}
+				else
+				{
+					newGrid[i][j] = 0;
+				}
+			}
+		}
+	}
+		
+	grid = newGrid.slice();
+}
+
+
+
+setInterval(update,20);
+
+
