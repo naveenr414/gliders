c996af2b2d6ea030e2b27fb243ed2acf2e1beb22
naveenr414 <dsfan414@gmail.com>
2018-08-03 13:15:00 -0400

    Cleaned up client.js

--- client.js
+++ client.js
@@ -1,20 +1,30 @@
-var grid = 0;
-var turn = false;
-var placed = 0;
-var totalPlace = 3;
+/* Says which player played down a block at each position on the grid */
+var grid = [];
+var playerNumber = 0;
+
+/* Block color for each player */ 
 var colors = [];
 
-var size = 30;
-var width = 300;
-var height = 300;
+var turn = false;
+var blocksPlaced = 0;
+const maxBlocksPlaced = 3;
+
+const blockSize = 30;
+const width = 300;
+const height = 300;
+const blocksHorizontal = width/blockSize;
+const blocksVertical = height/blockSize
+
+const hoverColor = "AAAAAA";
 
 var mouseX = 0;
 var mouseY = 0;
 var mousePress = false;
+
 var socket = io();
 
-var number = 0;
 
+/*Update the status of the mouse*/
 $(document).mousemove(function(e) {
     mouseX = e.pageX;
     mouseY = e.pageY;
@@ -28,17 +38,22 @@ $(document).mouseup(function(e){
 	mousePress=false;
 });
 
-function drawGrid()
-{
+function drawBlock(var gridX, var gridY, var c){
+	var startX = blockSize*x;
+	var startY = blockSize*y;
+	c.fillRect(gridX*blockSize,gridY*blockSize,blockSize,blockSize);
+}
+
+function drawGrid(){
 	var c = document.getElementById("mainCanvas").getContext("2d");	
 	c.clearRect(0, 0, width,height);
-	for(var y = 0;y<grid.length;y++)
-	{
-		for(var x = 0;x<grid.length;x++)
-		{	
+	
+	for(var y = 0;y<grid.length;y++){
+		for(var x = 0;x<grid.length;x++){	
+			/* Draw non-empty blocks */ 
 			if(grid[y][x]!=0){
 				c.fillStyle=colors[grid[y][x]];
-				c.fillRect(size*x,size*y,size,size);
+				drawBlock(x,y,c);
 			}
 		}
 	}
@@ -46,49 +61,41 @@ function drawGrid()
 	c.stroke();
 }
 
-function checkInput()
-{
-	console.log(turn);
-	if(turn)
-	{
-		drawGrid();
+function checkInput(){
+	if(turn){
 		var c = document.getElementById("mainCanvas").getContext("2d");	
-		var gridX = Math.floor(mouseX/size);
-		var gridY = Math.floor(mouseY/size);
+		drawGrid();
 		
-		if(gridX<=width/size && gridY<=height/size)
-		{
-			c.fillStyle="#AAAAAA";
-			c.fillRect(gridX*size,gridY*size,size,size);
-					
-			if(mousePress && grid[gridY][gridX] == 0)
-			{
+		/* Convert mouse position to grid position */ 
+		var gridX = Math.floor(mouseX/blockSize);
+		var gridY = Math.floor(mouseY/blockSize);
+
+		var gridFree = grid[gridY][gridX]==0;
+		if(gridX<=blocksHorizontal && gridY<=blocksVertical && gridFree){
+			/* Draw a block where we're hovering*/ 
+			c.fillStyle=hoverColor;
+			drawBlock(gridX,gridY,c);
+			
+			if(mousePress){
 				grid[gridY][gridX] = number;
-				placed+=1;
+				blocksPlaced+=1;
 				socket.emit ('move',[gridY,gridX,number]);
 				
-				console.log(placed+" "+totalPlace);
-				if(placed == totalPlace)
-				{
-					console.log("Done");
+				if(blocksPlaced == maxBlocksPlaced){
 					turn = false;
-				}
-				
-				console.log("Turn is "+turn);
-				
+				}				
 			}	
 		}
 	}
 }
 
-socket.on('grid',function(g){
+socket.on('gridUpdate',function(g){
 	grid = g;
 	drawGrid();
-	console.log("Grid recieved");
 });
 
+/* We recieve what this player's block color is */ 
 socket.on('color',function(c){
-
 	colors = c;
 	drawGrid();
 });
@@ -97,8 +104,9 @@ socket.on('input', function(){
 	turn = true;
 });
 
-socket.on('number', function(n){
-	number = n;
+/* We recieve what this player's number is */ 
+socket.on('playerNumber', function(n){
+	playerNumber = n;
 });
 
 setInterval(checkInput,20);
